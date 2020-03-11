4379829db81add22ac2f48577659351a161d7784
21807481 <hadrien.herubel@ens.uvsq.fr>
2020-03-11 11:19:29 +0100

    Etat initial

---
+++ src/main/java/uvsq21807481/UneClasseMetier.java
@@ -0,0 +1,14 @@
+package uvsq21807481;
+
+import java.time.LocalDateTime;
+
+public class UneClasseMetier {
+
+    public void uneMethodeMetier() {
+        System.out.println(LocalDateTime.now() + ": Début de uneMethodeMetier"); //log message
+
+        // Traitements metiers
+
+        System.out.println(LocalDateTime.now() + ": Fin de uneMethodeMetier"); //log message
+    }
+}
